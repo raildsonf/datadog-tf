77bf222c3d8ec48dc35af8efed1f7305e359808d
raildsonf <rfelipe17@outlook.com>
2023-10-21 16:56:42 -0300

    generic commit

--- variables.tf
+++ variables.tf
@@ -1,11 +1,10 @@
 variable "datadog_api_key" {
   type    = string
-  default = "1037f095e182cfc365e3825e5c8ada45"
 }
 variable "datadog_app_key" {
   type    = string
-  default = "8d0e946d9a3fee4d8737feb6a9ced5ed2f9e3cba"
 }
+
 variable "datadog_api_url" {
   type = string
   default = "https://us5.datadoghq.com/"
